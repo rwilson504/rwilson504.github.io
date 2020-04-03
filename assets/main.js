21b039d41dd24bec9430db9ecad5a726977e6869
Rick Wilson <me@richardawilson.com>
2020-04-03 15:26:22 -0400

    update

--- assets/main.js
+++ assets/main.js
@@ -42,9 +42,9 @@
       $.ajax({
         type: $(this).attr('method'),
         url:  $(this).attr('action'),
-        crossDomain: true,
+        //crossDomain: true,
         data: $(this).serialize(),
-        //headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"},                
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"},                
         contentType: 'application/x-www-form-urlencoded',
         success: function (data) {
           showModal('Comment submitted', 'Thanks! Your comment is <a href="https://github.com/rwilson504/rwilson504.github.io/pulls">pending</a>. It will appear when approved.');
